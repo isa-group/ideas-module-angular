de72ab9af7c80739ff3c5fb41cba1639e0b50314
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2016-02-11 12:01:10 +0100

    First commit

---
+++ languages/ideas-language-angular/src/main/java/es/us/isa/ideas/controller/angular/AngularLanguageController.java
@@ -0,0 +1,36 @@
+package es.us.isa.ideas.controller.angular;
+
+import es.us.isa.ideas.module.common.AppResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import es.us.isa.ideas.module.controller.BaseLanguageController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/language")
+public class AngularLanguageController extends BaseLanguageController {
+
+    @RequestMapping(value = "/operation/{id}/execute", method = RequestMethod.POST)
+    @ResponseBody
+    @Override
+    public AppResponse executeOperation(String id, String content, String fileUri, String auxArg0) {
+        return new AppResponse();
+    }
+
+    @RequestMapping(value = "/format/{format}/checkLanguage", method = RequestMethod.POST)
+    @ResponseBody
+    @Override
+    public AppResponse checkLanguage(String id, String content, String fileUri) {
+        return new AppResponse();
+    }
+
+    @RequestMapping(value = "/convert", method = RequestMethod.POST)
+    @ResponseBody
+    @Override
+    public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content) {
+        return new AppResponse();
+    }
+
+}
