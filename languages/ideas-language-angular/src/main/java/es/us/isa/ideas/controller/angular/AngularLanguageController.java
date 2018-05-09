3b9f472ca43f37af6fbb392ce213d87eeee07ad7
José Antonio Parejo Maestre <japarejo@gmail.com>
2018-05-09 19:41:59 +0200

    Added parameter to the language controllers to support the use of session according to the modifications in ideas-base-module

--- languages/ideas-language-angular/src/main/java/es/us/isa/ideas/controller/angular/AngularLanguageController.java
+++ languages/ideas-language-angular/src/main/java/es/us/isa/ideas/controller/angular/AngularLanguageController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import es.us.isa.ideas.module.controller.BaseLanguageController;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,21 +16,21 @@ public class AngularLanguageController extends BaseLanguageController {
     @RequestMapping(value = "/operation/{id}/execute", method = RequestMethod.POST)
     @ResponseBody
     @Override
-    public AppResponse executeOperation(String id, String content, String fileUri, String auxArg0) {
+    public AppResponse executeOperation(String id, String content, String fileUri, String auxArg,HttpServletRequest request) {
         return new AppResponse();
     }
 
     @RequestMapping(value = "/format/{format}/checkLanguage", method = RequestMethod.POST)
     @ResponseBody
     @Override
-    public AppResponse checkLanguage(String id, String content, String fileUri) {
+    public AppResponse checkLanguage(String id, String content, String fileUri,HttpServletRequest request) {
         return new AppResponse();
     }
 
     @RequestMapping(value = "/convert", method = RequestMethod.POST)
     @ResponseBody
     @Override
-    public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content) {
+    public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content,HttpServletRequest request) {
         return new AppResponse();
     }
 
